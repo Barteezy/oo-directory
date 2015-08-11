deaf661137ff9282e5e87d7800381d2f4a0fc2cc
Luke Bartel <bartelluke@gmail.com>
2015-08-11 09:16:55 -0600

    added dmv

---
+++ dmv/dmv.js
@@ -0,0 +1,41 @@
+
+var DMV = function (agents) {
+  this.agents = agents;
+  this.inLine = [];
+  this.attendingTo = {};
+  agents.forEach(function(agent){
+    this.attendingTo[agent] = null;
+  }.bind(this));
+  this.resolve = function(customer){
+    for(key in this.attendingTo){
+      if(this.attendingTo[key] === customer){
+        this.attendingTo[key] = null
+      }
+    }
+  }
+};
+
+DMV.prototype.customersInLine = function(){
+  return this.inLine;
+}
+
+DMV.prototype.enter = function(person){
+  this.inLine.push(person)
+}
+
+DMV.prototype.nextCustomer = function(){
+  for (var i = 0; i < this.agents.length; i++) {
+    var agent = this.agents[i]
+    if(this.attendingTo[agent] === null){
+      this.attendingTo[agent] = this.inLine.shift();
+      break
+    }
+  }
+}
+
+DMV.prototype.currentCustomerFor = function(agent){
+  return this.attendingTo[agent];
+}
+
+
+module.exports = DMV;
